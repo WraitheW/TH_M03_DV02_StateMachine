6e2c27026d9683df176bdfa03a072e33ed6ff584
WraitheW <48034189+WraitheW@users.noreply.github.com>
2020-10-01 21:55:10 -0700

    Add files via upload

---
+++ Scripts/SpinningState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningState : PlayerState
+{
+    public void Enter(Player player)
+    {
+        player.mCurrentState = new SpinningState();
+        player.transform.Rotate(Vector3.up * 500000 * Time.deltaTime);
+        Debug.Log("Enter Spinning State");
+    }
+
+    public void Execute(Player player)
+    {
+        if (Input.GetKeyUp(KeyCode.U))
+        {
+            StandingState standingState = new StandingState();
+            standingState.Enter(player);
+        }
+    }
+}
---
+++ Scripts/StandingState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandingState : PlayerState
+{
+
+    public void Enter(Player player)
+    {
+        player.mCurrentState = new StandingState();
+        Debug.Log("Enter Standing State");
+    }
+
+    public void Execute(Player player)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpingState jumpingState = new JumpingState();
+            jumpingState.Enter(player);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            DuckingState duckingState = new DuckingState();
+            duckingState.Enter(player);
+        }
+    }
+}
